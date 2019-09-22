d5e9f59f430c57cab2fc106a65309eb375a98d26
YLTsai0609 <yltsai0609@gmail.com>
2019-09-22 17:06:03 +0800

    clean the ignore

--- Solution.py
+++ Solution.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.4'
-#       jupytext_version: 1.1.3
+#       jupytext_version: 1.1.6
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
